7fd1d06594e89dc138ed62c3e14b657f877ee10e
Andrew van Duivenbode <andrew@creativetransmissions.com>
2023-01-05 18:18:45 +0000

    settings

--- src/lib/scene.js
+++ src/lib/scene.js
@@ -6,16 +6,23 @@ import * as D3D from 'd3d';
 		'amphitheater':{
 		hasCircleLayout: true,
 		radius: 20,
-		sceneryPath: '/models/scenery/vr_art_gallery_3_baked.glb',
+		sceneryPath: '/models/scenery/vr_art_gallery_9_two_octahedra_baked.glb',
 		sceneScale: 1.5,
-		playerStartPos: { x: -0, y: 2 ,z: 0 },
+		playerStartPos: { x: -4, y: 4 ,z: -5 },
 		}
 	}
 
   const config = {
+  // avatarPath:  '/models/avatars/vroid-anime-girl.vrm',
+    //avatarPath:  '/models/avatars/vroid-anime-girl2.vrm',
+    avatarPath:  '/models/avatars/vroid-anime-guy.vrm',
+
+
+  //  avatarPath:  '/models/avatars/ghostHQ.fbx',
+  //  avatarPath:  'https://desodata.azureedge.net/unzipped/afa02ebe8c02c6f8f22ca46031e048b69a6b2add303ffae538bb95bf6adb6db7/vrm/normal/European%2520Male1_High%2520Volume%2520Brushed%2520Up_Deso%2520Casual.vrm',
     walkSpeed: 5,
     defaultLoader: 'gltf',
-    firstPerson: true,    
+    firstPerson: false,    
    // imageProxyUrl: PROXYURL,
     ctrClass: 'space-ctr', // Attribute of div containing post and any additionl html
     linkText: 'View in 3D',
@@ -29,7 +36,7 @@ import * as D3D from 'd3d';
     scaleModelToHeight:2,
     scaleModelToWidth: 2,
     scaleModelToDepth: 2,   
-    playerStartPos: {x:0,y:3,z:0},  // location in the environment where the player will appear
+    playerStartPos: { x: -4, y: 4 ,z: -5 },  // location in the environment where the player will appear
     avatarSize: {width: 1, height:1, depth:1}, // Max dimensions of avatar
     vrType:'walking', // default to walking unless vrcontrols=flying is in url params
     sceneryOptions: sceneryOptions[sceneryName],
